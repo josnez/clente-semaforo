0009e1fafcc97094c658b8f4bca005252910dc80
Josue Nuñez Prada <josuenunezprada@gmail.com>
2022-10-04 18:16:34 -0500

    read thread and manage input byte

--- src/logica/Tarjeta.java
+++ src/logica/Tarjeta.java
@@ -1,14 +1,21 @@
 package logica;
 
+import presentacion.Modelo;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 
-public class Tarjeta {
+public class Tarjeta implements Runnable {
+
+    private Modelo modelo;
 
     private ArrayList<Semaforo> gprSemaforico1;
     private ArrayList<Semaforo> gprSemaforico2;
     private int identificador;
 
-    // Todo gestion de semaforos
     private String host = "localhost"; //127.0.0.1  , IP del servidor
     private int puerto = 5000;
     private Socket sock;
@@ -19,7 +26,8 @@ public class Tarjeta {
     private volatile boolean lecturaActiva;
     private Thread hiloLectura;
 
-    public TestSistemaCliente() {
+    public Tarjeta(Modelo modelo) {
+        this.modelo = modelo;
         lecturaActiva = true;
         hiloLectura = new Thread(this);
     }
@@ -43,5 +51,40 @@ public class Tarjeta {
         datosEntrada.close();
         sock.close();
 
+    }
+
+    @Override
+    public void run() {
+        // Para almacenar lo que llegue del servidor
+        byte buffer[];
+        int intData;
+        String binaryData;
+
+        while(lecturaActiva){
+            try {
+                // lee lo que envía el server
+                System.out.println("Esperando mensaje...");
+                /*buffer = new byte[500];
+                datosEntrada.read(buffer); // Se queda acá, hasta que el servidor envíe algo*/
+                intData = datosEntrada.readInt();
+                // decodificar el mensaje
+                binaryData = Integer.toBinaryString(intData);
+                System.out.print("El servidor envía: ");
+                System.out.println(intData + " " + binaryData);
+                // procesaro info
+                modelo.procesarInformacion(binaryData);
+
+                if(binaryData.toString().equals("ADIOS")){
+                    System.out.println("entra");
+                    this.lecturaActiva = false;
+                    break;
+                }else{
+                    System.out.println("Diferentes");
+                }
+
+            } catch (IOException ex) {
+                System.out.println("error en la comunicación");
+            }
+        }
     }
 }
--- src/presentacion/Modelo.java
+++ src/presentacion/Modelo.java
@@ -1,20 +1,121 @@
 package presentacion;
 
+import logica.Led;
+import logica.Semaforo;
 import logica.TestSistemaCliente;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Modelo {
 
-    private TestSistemaCliente tsc;
+    private Semaforo sistema1;
+    private Semaforo sistema2;
+    private Semaforo sistemaActivo;
+
+    private Ventana ventana;
+
     public void iniciar() {
 
-        // Todo arrancar conexion
-        try {
-            tsc.conectar();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
+    public Ventana getVentana() {
+        if (ventana == null) {
+            ventana = new Ventana(this);
+        }
+        return ventana;
+    }
+    public void procesarInformacion(String binaryData) {
+        // byte recibido |i|r|a|v|i|r|a|v|
+
+        while (binaryData.length() < 8) {
+            binaryData = "0" + binaryData;
+        }
+
+        // todo cambiar estado de leds
+
+    }
+
+    /*public void cambioEstadoRojo() {
+        Led ledRojo = (Led) sistemaActivo.getLeds().get(0);
+
+        if (ledRojo.getEstado() == 0) {
+            ledRojo.getLbl().setBackground(new Color(255, 0, 0));
+            ventana.getBtnRojo().setIcon(ventana.getImgOn());
+            comunicarEstado("estado botón rojo -> 1");
+            ledRojo.setEstado(1);
+        } else if (ledRojo.getEstado() == 1) {
+            ledRojo.getLbl().setBackground(new Color(186, 0, 0));
+            ventana.getBtnRojo().setIcon(ventana.getImgOff());
+            comunicarEstado("estado botón rojo -> 0");
+            ledRojo.setEstado(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "El LED esta dañado");
+            comunicarEstado("estado botón rojo -> dañado");
+        }
+
+    }
+
+    void cambioEstadoAmarillo() {
+        Led ledAmarillo = (Led) sistemaActivo.getLeds().get(1);
+
+        if (ledAmarillo.getEstado() == 0) {
+            ledAmarillo.getLbl().setBackground(new Color(255, 255, 0));
+            ventana.getBtnAmarillo().setIcon(ventana.getImgOn());
+            comunicarEstado("estado botón amarillo -> 1");
+            ledAmarillo.setEstado(1);
+        } else if (ledAmarillo.getEstado() == 1) {
+            ledAmarillo.getLbl().setBackground(new Color(186, 186, 0));
+            ventana.getBtnAmarillo().setIcon(ventana.getImgOff());
+            comunicarEstado("estado botón amarillo -> 0");
+            ledAmarillo.setEstado(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "El LED esta dañado");
+            comunicarEstado("estado botón amarillo -> dañado");
+        }
+
+    }
+
+    public void cambioEstadoVerde() {
+        Led ledVerde = (Led) sistemaActivo.getLeds().get(2);
+
+        if (ledVerde.getEstado() == 0) {
+            ledVerde.getLbl().setBackground(new Color(0, 204, 0));
+            ventana.getBtnVerde().setIcon(ventana.getImgOn());
+            comunicarEstado("estado botón verde -> 1");
+            ledVerde.setEstado(1);
+        } else if (ledVerde.getEstado() == 1) {
+            ledVerde.getLbl().setBackground(new Color(0, 145, 0));
+            ventana.getBtnVerde().setIcon(ventana.getImgOff());
+            comunicarEstado("estado botón verde -> 0");
+            ledVerde.setEstado(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "El LED esta dañado");
+            comunicarEstado("estado botón verde -> dañado");
+        }
+
+    }
+
+    public void comprobarEstadoBotones() {
+        ArrayList<Led> leds = sistemaActivo.getLeds();
+
+        if(leds.get(0).getEstado() == 0)
+            ventana.getBtnRojo().setIcon(ventana.getImgOff());
+        else
+            ventana.getBtnRojo().setIcon(ventana.getImgOn());
+
+        if(leds.get(1).getEstado() == 0)
+            ventana.getBtnAmarillo().setIcon(ventana.getImgOff());
+        else
+            ventana.getBtnAmarillo().setIcon(ventana.getImgOn());
+
+
+        if(leds.get(2).getEstado() == 0)
+            ventana.getBtnVerde().setIcon(ventana.getImgOff());
+        else
+            ventana.getBtnVerde().setIcon(ventana.getImgOn());
+
+    }*/
 }
